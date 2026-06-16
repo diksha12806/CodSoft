991e2706ae61753af30e0ee77c85914efb624173
diksha12806 <dikshadavkar12806@gmail.com>
2026-06-16 16:19:27 +0530

    Delete ProjectManager/script.js

--- ProjectManager/script.js
+++
@@ -1,9 +0,0 @@
-function addTask() {
-  let project = document.getElementById("project").value;
-  let task = document.getElementById("task").value;
-  let date = document.getElementById("date").value;
-
-  let li = document.createElement("li");
-  li.innerText = project + " - " + task + " (Deadline: " + date + ")";
-  document.getElementById("taskList").appendChild(li);
-}
